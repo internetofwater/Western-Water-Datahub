f8bc76381711d841d12072e4d16ebbc543cbb229
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-06-30 16:36:00 -0400

    Snotel huc fixes

--- packages/snotel/snotel/lib/locations.py
+++ packages/snotel/snotel/lib/locations.py
@@ -16,14 +16,37 @@ from typing import Optional, cast
 type longitudeAndLatitude = tuple[float, float]
 
 
+def get_and_remove_huc_from_properties(
+    properties: list[tuple[str, str]],
+) -> Optional[str]:
+    """
+    Get the huc from the properties list if it is present and remove it from the list
+    We have to remove it from the list because it is a special filter and we don't want to
+    double filter on it
+    """
+    for i, (key, value) in enumerate(properties):
+        if key == "huc":
+            del properties[i]
+            return value
+    return None
+
+
 class SnotelLocationCollection(LocationCollection, LocationCollectionProtocolWithEDR):
     """A wrapper class containing locations and methods to filter them"""
 
-    def __init__(self, select_properties: Optional[list[str]] = None):
+    def __init__(
+        self, select_properties: Optional[list[str]] = None, huc: Optional[str] = None
+    ):
         self.cache = RedisCache()
         # snotel also proxies usgs so we just want to get SNOTEL stations
         JUST_SNOTEL_STATIONS = "*:*:SNTL"
         url = f"https://wcc.sc.egov.usda.gov/awdbRestApi/services/v1/stations?activeOnly=true&stationTriplets={JUST_SNOTEL_STATIONS}"
+
+        # filter by the hydrological unit code. this can container a wildcard. i.e. 12* matches all hucs beginning with 12
+        if huc:
+            # NOTE this is 'hucs' not 'huc', if you accidentally misspell it snotel won't throw an error and will just ignore the filter
+            url += f"&hucs={huc}"
+
         if select_properties:
             url += f"&elements={','.join(select_properties)}"
         result = await_(self.cache.get_or_fetch_json(url))
