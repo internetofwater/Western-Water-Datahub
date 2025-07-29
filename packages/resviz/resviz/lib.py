fbf8d41f02f6cc3a7ebf1ba5f4fb522da7a62c6c
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2025-07-29 19:16:12 -0400

    Use separate table for monitoring location and parameter (#125)
    * Use separate table for monitoring location and parameter
    
    * Update pygeoapi.config.yml

--- packages/resviz/resviz/lib.py
+++ packages/resviz/resviz/lib.py
@@ -8,7 +8,7 @@ import re
 import requests
 from typing import Iterator
 
-from resviz.mappings import DOI_REGIONS, LOCATION_IDS
+from resviz.mappings import LOCATION_IDS
 
 
 gdal.UseExceptions()
@@ -33,51 +33,27 @@ def date_range(start_date: date, end_date: date) -> Iterator[date]:
 
 def create_feature(pg_layer, row, parameter: str):
     """Create postgres feature from a CSV row"""
-    p_name = "Lake/Reservoir Storage"
 
     match parameter:
         case "p10":
-            p_name += " 10th Percentile"
             p_val = "DataDateP10"
-            p_suffix = "P10"
         case "avg":
-            p_name += " Average"
             p_val = "DataDateAvg"
-            p_suffix = "avg"
         case "p90":
-            p_name += " 90th Percentile"
             p_val = "DataDateP90"
-            p_suffix = "P90"
         case _:
             p_val = "DataValue"
-            p_suffix = "raw"
 
     row["SiteShortName"] = re.search(r"/([^/]+)\.png$", row["TeacupUrl"]).group(1)  # pyright: ignore
     row["SiteId"] = LOCATION_IDS.get(row["SiteShortName"], 0)
-    region = DOI_REGIONS[row["DoiRegion"]]
 
     feature = ogr.Feature(pg_layer.GetLayerDefn())
-    id = f"{row['SiteId']}.{row['DataDate']}.{p_suffix}"
+    id = f"{row['SiteId']}.{row['DataDate']}.{parameter}"
     feature.SetField("id", id)
-    feature.SetField("monitoring_location_id", row["SiteId"])
-    feature.SetField("monitoring_location_name", row["SiteShortName"])
-    feature.SetField("site_name", row["SiteName"])
-    feature.SetField("state", row["State"])
-    feature.SetField("doi_region_num", region["reg_num"])
-    feature.SetField("doi_region_name", region["reg_name"])
-    feature.SetField("doi_region", row["DoiRegion"])
-    feature.SetField("huc08", row["Huc8"])
-    feature.SetField("huc06", row["Huc8"][:6])
     feature.SetField("value", row[p_val])
-    feature.SetField("max_capacity", row["MaxCapacity"])
-    feature.SetField("parameter_unit", row["DataUnits"].lower())
     feature.SetField("data_date", row["DataDate"])
-    feature.SetField("parameter_name", p_name)
+    feature.SetField("monitoring_location_id", row["SiteId"])
     feature.SetField("parameter_id", parameter)
-
-    point = ogr.Geometry(ogr.wkbPoint)
-    point.SetPoint_2D(0, float(row["Lon"]), float(row["Lat"]))
-    feature.SetGeometry(point)
 
     try:
         pg_layer.CreateFeature(feature)
