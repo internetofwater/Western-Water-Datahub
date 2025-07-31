cd2a884ff5f5d3518039e6b1f2e7bf1fd1a7a8bd
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-07-31 14:07:03 -0400

    precommit

--- packages/resops/resops/lib.py
+++ packages/resops/resops/lib.py
@@ -1,3 +1,6 @@
+# Copyright 2025 Lincoln Institute of Land Policy
+# SPDX-License-Identifier: MIT
+
 from collections import OrderedDict
 import datetime
 from typing import Optional, TypedDict, cast
