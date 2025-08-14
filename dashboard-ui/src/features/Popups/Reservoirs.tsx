ada8dd67a33372a9756f2e4eab7a85c62522a537
John Salman <johnfish15@gmail.com>
2025-08-14 12:50:38 -0700

    Fix build errors, minor style bug

--- dashboard-ui/src/features/Popups/Reservoirs.tsx
+++ dashboard-ui/src/features/Popups/Reservoirs.tsx
@@ -5,9 +5,8 @@
 
 import { GeoJsonProperties } from 'geojson';
 import { ReservoirConfig } from '@/features/Map/types';
-import { Card, Title, Text, Group, Stack } from '@mantine/core';
+import { Card, Title, Text, Stack } from '@mantine/core';
 import styles from '@/features/Popups/Popups.module.css';
-import { TextBlock } from '@/components/TextBlock';
 import { Graphic } from '@/features/Reservior/TeacupDiagram/Graphic';
 import useMainStore from '@/lib/main';
 import dayjs from 'dayjs';
@@ -25,14 +24,6 @@ export const ReservoirPopup: React.FC<Props> = (props) => {
     if (!reservoirProperties) {
         return null;
     }
-
-    const storage = Number(reservoirProperties[config.storageProperty]);
-    const capacity = Number(reservoirProperties[config.capacityProperty]);
-    const average = Number(
-        reservoirProperties[config.thirtyYearAverageProperty]
-    );
-    const percentFull = ((storage / capacity) * 100).toFixed(1);
-    const percentOfAverage = ((storage / average) * 100).toFixed(1);
 
     return (
         <Card
@@ -64,58 +55,6 @@ export const ReservoirPopup: React.FC<Props> = (props) => {
                 />
                 <Text size="sm">Click to Learn More</Text>
             </Stack>
-
-            {/* <Group justify="space-between" wrap="nowrap">
-                <Graphic
-                    reservoirProperties={reservoirProperties}
-                    config={config}
-                    showLabels={false}
-                    labels={false}
-                    listeners={false}
-                    colorScheme={colorScheme}
-                />
-                <TextBlock>
-                    <Group gap="xs" justify="flex-start">
-                        <Text size="sm" fw={700}>
-                            Capacity:
-                        </Text>
-                        <Text size="sm">
-                            {capacity.toLocaleString('en-US')}&nbsp;acre-feet
-                        </Text>
-                    </Group>
-                    <Group gap="xs" justify="flex-start">
-                        <Text size="sm" fw={700}>
-                            Storage:
-                        </Text>
-                        <Text size="sm">
-                            {storage.toLocaleString('en-US')}&nbsp;acre-feet
-                        </Text>
-                    </Group>
-                    <Group gap="xs" justify="flex-start">
-                        <Text size="sm" fw={700}>
-                            Average:
-                        </Text>
-                        <Text size="sm">
-                            {average.toLocaleString('en-US')}&nbsp;acre-feet
-                        </Text>
-                    </Group>
-                    <Group gap="xs" justify="flex-start">
-                        <Text size="sm" fw={700}>
-                            Percent Full:
-                        </Text>
-                        <Text size="sm">{percentFull}%</Text>
-                    </Group>
-                    <Group gap="xs" justify="flex-start">
-                        <Text size="sm" fw={700}>
-                            Percent of Average:
-                        </Text>
-                        <Text size="sm">{percentOfAverage}%</Text>
-                    </Group>
-                </TextBlock>
-            </Group> 
-            <Text size="sm" mx="auto">
-                Click to Learn More
-            </Text>*/}
         </Card>
     );
 };
