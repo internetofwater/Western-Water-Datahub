f689597823162ffef9712b8e75c1ae7ebc8a759d
John Salman <johnfish15@gmail.com>
2025-08-25 09:00:11 -0700

    Deploy test

--- hub-ui/src/managers/__tests__/Main.test.ts
+++ hub-ui/src/managers/__tests__/Main.test.ts
@@ -6,9 +6,6 @@
 import { describe, expect, test } from 'vitest';
 
 // import { createStore, StoreApi, UseBoundStore } from 'zustand';
-// import MainController from '@/managers/Main.manager';
-// import useMainStore from '@/stores/main';
-// import { DatasourceType, MainState } from '@/stores/main/types';
 // import MainManager from '@/managers/Main.manager';
 
 describe('MainController', () => {
