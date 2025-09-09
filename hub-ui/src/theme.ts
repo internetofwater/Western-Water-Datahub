2a256c3162a061ba93009cbeef7f2ef08d3c8efe
John Salman <johnfish15@gmail.com>
2025-09-09 11:57:14 -0700

    Feature: correct requests to leverage cache, USBR colors, added tooltip text

--- hub-ui/src/theme.ts
+++ hub-ui/src/theme.ts
@@ -1,13 +1,35 @@
-/**
- * Copyright 2025 Lincoln Institute of Land Policy
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { createTheme } from "@mantine/core";
+import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  fontFamily: "Arial, Helvetica, sans-serif",
+  fontFamily: 'Arial, Helvetica, sans-serif',
   headings: {
-    fontFamily: "Arial, Helvetica, sans-serif",
+    fontFamily: 'Arial, Helvetica, sans-serif',
+  },
+  primaryColor: 'spillway-blue',
+  colors: {
+    'spillway-blue': [
+      '#ffffff',
+      '#d4eaef',
+      '#aad5e0',
+      '#80c0d0',
+      '#55abc0',
+      '#2a96b1',
+      '#0081a1',
+      '#006c86',
+      '#00566b',
+      '#004050',
+    ],
+    'red-rocks': [
+      '#ffffff',
+      '#f0dede',
+      '#e0bcbc',
+      '#d09b9b',
+      '#c17a7a',
+      '#b25858',
+      '#a23737',
+      '#872e2e',
+      '#6c2525',
+      '#511c1c',
+    ],
   },
 });
