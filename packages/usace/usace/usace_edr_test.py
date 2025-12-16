0304c77b68a771692f75cbb5dc78fb82e83e1f6f
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-12-16 19:21:06 +0000

    Switch USACE parameters to be queried and return the same names (#231)
    Switch USACE names to be queried and return the same name

--- packages/usace/usace/usace_edr_test.py
+++ packages/usace/usace/usace_edr_test.py
@@ -17,12 +17,16 @@ def test_usace_decode_param():
     p = USACEEDRProvider(provider_def)
     result = p.locations(
         location_id="2796555126",
-        select_properties=["Stage"],
+        select_properties=["Stage Tailwater"],
         datetime_="2025-01-01/..",
     )
     assert result and "coverages" in result
     assert len(result["coverages"]) == 1, (
         "There should be one coverage for this location after filtering out the others"
+    )
+    assert "parameters" in result
+    assert "Stage Tailwater" in result["parameters"], (
+        "Expected to find the filtered parameter with the same name in the parameters list"
     )
 
     with pytest.raises(ProviderNoDataError):
