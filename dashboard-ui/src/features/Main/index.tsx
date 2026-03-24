ed00555859497138844c5da42f6ddbc6d0d76ffe
John Salman <johnfish15@gmail.com>
2026-03-23 17:20:07 -0700

    Show notification indicating application is still in development

--- dashboard-ui/src/features/Main/index.tsx
+++ dashboard-ui/src/features/Main/index.tsx
@@ -5,7 +5,7 @@
 
 'use client';
 
-import { Box, Group, Stack } from '@mantine/core';
+import { Box, Group, Stack, Notification, Text } from '@mantine/core';
 import Map from '@/features/Map';
 import styles from '@/features/Main/Main.module.css';
 import { MapTools } from '@/features/MapTools';
@@ -36,6 +36,22 @@ const Main: React.FC<Props> = (props) => {
                     <Panel accessToken={accessToken} />
                     <Stack gap={0} className={styles.right}>
                         <Map accessToken={accessToken} />
+                        <Box
+                            className={`${styles.container} ${styles.left} ${styles.top}`}
+                        >
+                            <Notification
+                                classNames={{
+                                    description: styles.betaDescription,
+                                }}
+                                title={<Text fw={700}>Application Beta</Text>}
+                            >
+                                <Text size="sm">
+                                    This application is still in active
+                                    development. Some features may not be fully
+                                    implemented.
+                                </Text>
+                            </Notification>
+                        </Box>
                         <Box
                             className={`${styles.container} ${styles.left} ${styles.bottom}`}
                         >
