35e6f90f1b4515d3fcdc770ea12bbd0889e2734f
Benjamin Webb <benjamin.miller.webb@gmail.com>
2026-03-27 23:20:51 -0400

    Fix pyright

--- packages/resviz/resviz/__init__.py
+++ packages/resviz/resviz/__init__.py
@@ -54,13 +54,16 @@ def run_subprocess(csv_url: str):
 
     # Process source 'layer'
     for feature in layer:
+        row = {}
         try:
             row = {
                 layer_def.GetFieldDefn(i).GetName().strip(): feature.GetField(i).strip()
                 for i in range(layer_def.GetFieldCount())
             }
         except AttributeError:
-            LOGGER.error(f"Skipping row with missing fields from {row['SiteName']}")
+            LOGGER.error(
+                f"Skipping row with missing fields from {row.get('SiteName', 'unknown site')}"
+            )
             continue
 
         row["DataDate"] = datetime.strptime(row["DataDate"], "%m/%d/%Y").strftime(
