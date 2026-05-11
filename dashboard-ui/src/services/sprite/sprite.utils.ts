3308ce95e42e4011487b95f1175d747093955c59
John Salman <johnfish15@gmail.com>
2026-05-11 16:13:20 -0700

    Optimized teacup rendering approach

--- dashboard-ui/src/services/sprite/sprite.utils.ts
+++ dashboard-ui/src/services/sprite/sprite.utils.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 
+import { Map as MapboxMap } from 'mapbox-gl';
 import {
     ELevel,
     TCoordinateMap,
@@ -83,5 +84,114 @@ export const loadImageFile = (url: string): Promise<HTMLImageElement> => {
         image.src = url;
         image.onload = () => resolve(image);
         image.onerror = () => reject(new Error('Canvas failed to load'));
+    });
+};
+
+export const customLoader = (
+    map: MapboxMap,
+    coordinateMap: TCoordinateMap,
+    context: OffscreenCanvasRenderingContext2D
+) => {
+    const blockingSet = new Set<string>();
+    const cache = new Map<string, ImageData>();
+
+    const teacupWidth = 160;
+    const bufferedTeacupWidth = teacupWidth + 1;
+
+    const teacupHeight = 107;
+    const bufferedTeacupHeight = teacupHeight + 1;
+
+    // Width: 162 (160 + 2px buffer) x 20 column = 3240
+    // Height: 109 (107 + 2px buffer) x 25 rows = 2725
+    const tempCanvas = new OffscreenCanvas(3240, 2725);
+    const tempCtx = tempCanvas.getContext('2d', { willReadFrequently: true });
+
+    const overlayCanvas = new OffscreenCanvas(3240, 2725);
+    const overlayCtx = overlayCanvas.getContext('2d', {
+        willReadFrequently: true,
+    });
+
+    if (!tempCtx || !overlayCtx) {
+        return;
+    }
+
+    let i = 0,
+        j = 0;
+    map.on('styleimagemissing', (e) => {
+        const { id } = e;
+        if (!blockingSet.has(id)) {
+            blockingSet.add(id);
+            if (cache.has(id)) {
+                const imageData = cache.get(id);
+                if (imageData && !map.hasImage(id)) {
+                    map.addImage(id, imageData);
+                }
+            } else {
+                // We dont need the prefix
+                const [, storage, average] = id.split('-');
+
+                const teacupPosition =
+                    coordinateMap.sprites[`teacup-${storage}`];
+                const averagePosition =
+                    coordinateMap.sprites[`average-line-${average}`];
+
+                if (teacupPosition && averagePosition) {
+                    const {
+                        x: teacupX,
+                        y: teacupY,
+                        width: teacupWidth,
+                        height: teacupHeight,
+                    } = teacupPosition;
+                    const teacupImageData = context.getImageData(
+                        teacupX,
+                        teacupY,
+                        teacupWidth,
+                        teacupHeight
+                    );
+
+                    const {
+                        x: averageX,
+                        y: averageY,
+                        width: averageWidth,
+                        height: averageHeight,
+                    } = averagePosition;
+                    const averageImageData = context.getImageData(
+                        averageX,
+                        averageY,
+                        averageWidth,
+                        averageHeight
+                    );
+
+                    const row = 160 + bufferedTeacupWidth * i;
+                    const height = 107 + bufferedTeacupHeight * j;
+
+                    tempCtx.putImageData(teacupImageData, row, height);
+
+                    overlayCtx.putImageData(averageImageData, row, height);
+                    tempCtx.drawImage(overlayCanvas, 0, 0);
+
+                    // At row 20, move to next column
+                    if (i % 20 === 0) {
+                        j += 1;
+                    }
+                    i += 1;
+
+                    const imageData = tempCtx.getImageData(
+                        row,
+                        height,
+                        teacupWidth,
+                        teacupHeight
+                    );
+
+                    if (!map.hasImage(id)) {
+                        map.addImage(id, imageData);
+                    }
+                }
+            }
+        }
+    });
+
+    map.on('style.load', () => {
+        blockingSet.clear();
     });
 };
