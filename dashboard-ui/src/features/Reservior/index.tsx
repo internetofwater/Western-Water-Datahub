9731668d3344bd2644bc954e14724660a4f3299c
John Salman <johnfish15@gmail.com>
2026-06-11 12:34:09 -0700

    Add loading states for ui elements in the detailed modal

--- dashboard-ui/src/features/Reservior/index.tsx
+++ dashboard-ui/src/features/Reservior/index.tsx
@@ -3,7 +3,14 @@
  * SPDX-License-Identifier: MIT
  */
 import { useRef } from 'react';
-import { ActionIcon, Divider, Group, Modal, Text } from '@mantine/core';
+import {
+    ActionIcon,
+    Divider,
+    Group,
+    Modal,
+    Text,
+    Tooltip,
+} from '@mantine/core';
 import { useEffect, useState } from 'react';
 import {
     ReservoirConfigId,
@@ -37,6 +44,7 @@ import { Properties } from '@/components/Map/types';
 import Reset from '@/icons/Reset';
 import { TeacupReservoirField } from '@/features/Map/types/reservoir/teacup';
 import { ExtendedProperties } from './Info/Metrics';
+import { isDataValid } from './utils';
 
 /**
  *
@@ -66,6 +74,9 @@ const Reservoir: React.FC = () => {
     const [config, setConfig] = useState<ReservoirConfigProperties>();
     const [currentDate, setCurrentDate] = useState(reservoirDate);
     const [isLocation, setIsLocation] = useState(false);
+    const [title, setTitle] = useState('Reservoir');
+    const [shortTitle, setShortTitle] = useState('Reservoir');
+    const [dataIsValid, setDataIsValid] = useState(false);
 
     const controller = useRef<AbortController>(null);
     const isMounted = useRef(true);
@@ -166,6 +177,20 @@ const Reservoir: React.FC = () => {
                     'Failed to update reservoir storage data:',
                     error
                 );
+                setCurrentReservoirProperties({
+                    [config.longLabelProperty]: String(
+                        initialReservoirProperties[config.longLabelProperty]
+                    ),
+                    [TeacupReservoirField.UseTotalOrActiveStorage]: String(
+                        initialReservoirProperties[
+                            TeacupReservoirField.UseTotalOrActiveStorage
+                        ]
+                    ),
+                    source_name: initialReservoirProperties.source_name,
+                    source_uri: initialReservoirProperties.source_uri,
+                    wwdh_collection: initialReservoirProperties.wwdh_collection,
+                    wwdh_fid: initialReservoirProperties.wwdh_fid,
+                } as GeoJsonProperties & ExtendedProperties);
             } else if (
                 (error as Error)?.message &&
                 !(error as Error)?.message.includes('AbortError')
@@ -271,6 +296,25 @@ const Reservoir: React.FC = () => {
         }
     }, [overlay]);
 
+    useEffect(() => {
+        if (!currentReservoirProperties || !config) {
+            return;
+        }
+
+        const title = String(
+            currentReservoirProperties[config.longLabelProperty] ?? 'Reservoir'
+        );
+        setTitle(title);
+
+        const shortTitle = String(
+            currentReservoirProperties[config.shortLabelProperty] ?? 'Reservoir'
+        );
+        setShortTitle(shortTitle);
+
+        const dataIsValid = isDataValid(currentReservoirProperties, config);
+        setDataIsValid(dataIsValid);
+    }, [currentReservoirProperties]);
+
     const handleSetToDefault = () => {
         const today = dayjs().format('YYYY-MM-DD');
 
@@ -334,15 +378,13 @@ const Reservoir: React.FC = () => {
             }}
             opened={opened}
             onClose={handleClose}
-            title={
-                String(currentReservoirProperties[config.longLabelProperty]) ??
-                ''
-            }
+            title={title}
         >
             <>
                 <Info
                     reservoirProperties={currentReservoirProperties}
                     config={config}
+                    isLoading={isFetchingSingleReservoir}
                 />
                 {isLocation && (
                     <Group gap="var(--default-spacing)" align="flex-end">
@@ -351,21 +393,32 @@ const Reservoir: React.FC = () => {
                             className={styles.dateSelector}
                             valueFormat="MM/DD/YYYY"
                             disabled={isFetchingSingleReservoir}
+                            description="View historical data for this reservoir"
                             value={
                                 currentDate
                                     ? dayjs(currentDate).toDate()
                                     : undefined
                             }
                             maxDate={new Date()}
-                            label="Reservoir Storage Date"
+                            label={`${shortTitle} Storage Date`}
                             onChange={debouncedHandleDateChange}
                         />
-                        <ActionIcon
-                            classNames={{ icon: styles.actionIcon }}
-                            onClick={handleSetToDefault}
+                        <Tooltip
+                            label={
+                                reservoirDate
+                                    ? 'Reset data to the selected date'
+                                    : 'Reset data to the latest available'
+                            }
+                            position="top-start"
                         >
-                            <Reset />
-                        </ActionIcon>
+                            <ActionIcon
+                                classNames={{ icon: styles.actionIcon }}
+                                onClick={handleSetToDefault}
+                                disabled={isFetchingSingleReservoir}
+                            >
+                                <Reset />
+                            </ActionIcon>
+                        </Tooltip>
                     </Group>
                 )}
 
@@ -377,6 +430,7 @@ const Reservoir: React.FC = () => {
                         ref={chartRef}
                         config={config}
                         source={reservoir.source}
+                        isDataValid={dataIsValid}
                     />
                 ) : (
                     <Text ta={'center'} mt="14%">
