06c0db5f00c642065a0aa6dbd23176e3eeae1047
John Salman <31483839+john-salman@users.noreply.github.com>
2026-07-22 13:09:44 -0700

    Update link in FAQ (#428)
    * Update link in FAQ
    
    * rm console.log

--- dashboard-ui/src/features/Map/config.tsx
+++ dashboard-ui/src/features/Map/config.tsx
@@ -965,8 +965,6 @@ export const getLayerHoverFunction = (
                         return;
                     }
 
-                    console.log('e.features', e.features);
-
                     const zoom = map.getZoom();
                     if (zoom < 12) {
                         const feature = e.features?.[0] as
