1d7dc6cacde54671afb4f6633f76257c62127c93
Benjamin Webb <benjamin.miller.webb@gmail.com>
2026-08-05 13:59:59 -0400

    Fix pyright

--- packages/teacup/teacup/env.py
+++ packages/teacup/teacup/env.py
@@ -20,4 +20,4 @@ POSTGRES_URL = str(
     f"PG:dbname={POSTGRES_DB} host={POSTGRES_HOST} port={POSTGRES_PORT} user={POSTGRES_USER} password={POSTGRES_PASSWORD}"  # noqa
 )
 
-OGR_BATCH_SIZE = os.getenv("OGR_BATCH_SIZE", 1000)
+OGR_BATCH_SIZE = int(os.getenv("OGR_BATCH_SIZE", 1000))
