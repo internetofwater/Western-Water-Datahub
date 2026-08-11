4e8119cb39f059ecfda20ab59bd2795e0d78eff7
Emily DuBois <emilymdubois@gmail.com>
2026-08-11 06:20:28 -0500

    chore(ui): Add CGS contact card, update format

--- dashboard-ui/src/features/Help/consts.tsx
+++ dashboard-ui/src/features/Help/consts.tsx
@@ -7,6 +7,7 @@ import { ReactNode } from 'react';
 import {
     Anchor,
     AnchorProps,
+    Image,
     List,
     Stack,
     Text,
@@ -952,29 +953,76 @@ export const questions: QA[] = [
 type Contact = {
     id: string;
     image?: ReactNode;
+    role: string;
+    featured?: boolean;
     body: ReactNode;
-    link?: string;
+    link?: {
+        label: string;
+        href: string;
+    };
 };
 
 export const contacts: Contact[] = [
     {
-        id: 'github',
-        image: <GitHub />,
-        body: 'Access the repository containing the source code for the Western Water Datahub. Contribute new features, report issues, and learn more about how this application was built.',
-        link: 'https://github.com/internetofwater/Western-Water-Datahub',
+        id: 'cgs',
+        image: <Image src="/cgs-logo-color.png" />,
+        role: 'Design & Development',
+        featured: true,
+        body: (
+            <Text {...content}>
+                This application follows{' '}
+                <Anchor
+                    href="https://internetofwater.org/internet-of-water-principles/"
+                    c="blue.8"
+                >
+                    Internet of Water principles
+                </Anchor>{' '}
+                for interoperable water data. Application design and development
+                provided by the{' '}
+                <Anchor href="https://cgsearth.org/" c="blue.8">
+                    Center for Geospatial Solutions
+                </Anchor>{' '}
+                at the{' '}
+                <Anchor href="https://www.lincolninst.edu/" c="blue.8">
+                    Lincoln Institute of Land Policy
+                </Anchor>
+                .
+            </Text>
+        ),
     },
     {
         id: 'email-bor',
+        image: <Image src="/BofR-logo-dark.png" />,
+        role: 'Questions & Feedback',
         body: (
             <Text {...content}>
                 For questions or feedback on the Western Water Data Dashboard,
                 please contact the Bureau of Reclamation at{' '}
-                <Text {...content} c="blue" span>
+                <Anchor {...content} href="data@usbr.gov" c="blue.8">
                     data@usbr.gov
-                </Text>
+                </Anchor>
                 .
             </Text>
         ),
-        link: 'mailto:data@usbr.gov',
+        link: {
+            label: 'Email Bureau of Reclamation',
+            href: 'mailto:data@usbr.gov',
+        },
+    },
+    {
+        id: 'github',
+        image: <GitHub />,
+        role: 'Source Code & Contributions',
+        body: (
+            <Text {...content}>
+                Access the repository containing the source code for the Western
+                Water Datahub. Contribute new features, report issues, and learn
+                more about how this application was built.
+            </Text>
+        ),
+        link: {
+            label: 'View Repository',
+            href: 'https://github.com/internetofwater/Western-Water-Datahub',
+        },
     },
 ];
