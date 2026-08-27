da344848be716c1a09b532e03c667d5948961071
John Salman <johnfish15@gmail.com>
2026-08-27 07:53:04 -0700

    Remove comment code

--- hub-ui/src/features/Legend/Entry.tsx
+++ hub-ui/src/features/Legend/Entry.tsx
@@ -3,64 +3,47 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { useEffect, useState } from "react";
-import { ExpressionSpecification } from "mapbox-gl";
-import { ColorInput, Group, Stack, Text } from "@mantine/core";
-import { Grid } from "@/features/Legend/Grid";
-import styles from "@/features/Legend/Legend.module.css";
-import { OpacitySlider } from "@/features/Legend/OpacitySlider";
-import { Shapes } from "@/features/Legend/Shapes";
-import { VisibilityToggle } from "@/features/Legend/VisibilityToggle";
-import { DetailedGradient } from "@/features/Panel/Datasources/Palette/DetailedGradient";
-import mainManager from "@/managers/Main.init";
-import { TLayer } from "@/stores/main/types";
-import { CollectionType, getCollectionType } from "@/utils/collection";
+import { useEffect, useState } from 'react';
+import { ExpressionSpecification } from 'mapbox-gl';
+import { ColorInput, Group, Stack, Text } from '@mantine/core';
+import { Grid } from '@/features/Legend/Grid';
+import styles from '@/features/Legend/Legend.module.css';
+import { OpacitySlider } from '@/features/Legend/OpacitySlider';
+import { Shapes } from '@/features/Legend/Shapes';
+import { VisibilityToggle } from '@/features/Legend/VisibilityToggle';
+import { DetailedGradient } from '@/features/Panel/Datasources/Palette/DetailedGradient';
+import mainManager from '@/managers/Main.init';
+import { TLayer } from '@/stores/main/types';
+import { CollectionType, getCollectionType } from '@/utils/collection';
 
 type Props = {
   layer: TLayer;
-  handleColorChange: (color: TLayer["color"], layerId: TLayer["id"]) => void;
-  handleVisibilityChange: (visible: boolean, layerId: TLayer["id"]) => void;
-  handleOpacityChange: (
-    opacity: TLayer["opacity"],
-    layerId: TLayer["id"],
-  ) => void;
+  handleColorChange: (color: TLayer['color'], layerId: TLayer['id']) => void;
+  handleVisibilityChange: (visible: boolean, layerId: TLayer['id']) => void;
+  handleOpacityChange: (opacity: TLayer['opacity'], layerId: TLayer['id']) => void;
 };
 
 export const Entry: React.FC<Props> = (props) => {
-  const {
-    layer,
-    handleColorChange,
-    handleVisibilityChange,
-    handleOpacityChange,
-  } = props;
+  const { layer, handleColorChange, handleVisibilityChange, handleOpacityChange } = props;
 
-  const [collectionType, setCollectionType] = useState<CollectionType>(
-    CollectionType.Unknown,
-  );
-  const [title, setTitle] = useState("");
+  const [collectionType, setCollectionType] = useState<CollectionType>(CollectionType.Unknown);
+  const [title, setTitle] = useState('');
 
   useEffect(() => {
     const collection = mainManager.getCollection(layer.collectionId);
 
     if (collection) {
-      setTitle(collection?.title ?? "");
+      setTitle(collection?.title ?? '');
       const collectionType = getCollectionType(collection);
       setCollectionType(collectionType);
     }
   }, [layer]);
 
-  const showColors = [
-    CollectionType.EDR,
-    CollectionType.EDRGrid,
-    CollectionType.Features,
-  ].includes(collectionType);
-  const showShapes = [CollectionType.EDR, CollectionType.Features].includes(
-    collectionType,
+  const showColors = [CollectionType.EDR, CollectionType.EDRGrid, CollectionType.Features].includes(
+    collectionType
   );
+  const showShapes = [CollectionType.EDR, CollectionType.Features].includes(collectionType);
   const showGrid = [CollectionType.EDRGrid].includes(collectionType);
-  // const showOpacity = [CollectionType.EDRGrid, CollectionType.Map].includes(
-  //   collectionType,
-  // );
 
   return (
     <Stack w="100%" gap="xs" className={styles.legendEntry}>
@@ -79,17 +62,15 @@ export const Entry: React.FC<Props> = (props) => {
         opacity={layer.opacity}
         handleOpacityChange={handleOpacityChange}
       />
-      {/* {showOpacity && (
-      )} */}
 
-      {layer.paletteDefinition && typeof layer.color !== "string" ? (
+      {layer.paletteDefinition && typeof layer.color !== 'string' ? (
         <DetailedGradient
           collectionId={layer.collectionId}
           color={layer.color as ExpressionSpecification}
           paletteDefinition={layer.paletteDefinition}
         />
       ) : (
-        typeof layer.color === "string" && (
+        typeof layer.color === 'string' && (
           <Group w="100%" justify="space-between" align="flex-start">
             <Stack justify="flex-start">
               {showColors && (
@@ -105,9 +86,7 @@ export const Entry: React.FC<Props> = (props) => {
                 />
               )}
             </Stack>
-            {showShapes && (
-              <Shapes color={layer.color} geometryTypes={layer.geometryTypes} />
-            )}
+            {showShapes && <Shapes color={layer.color} geometryTypes={layer.geometryTypes} />}
             {showGrid && <Grid color={layer.color} />}
           </Group>
         )
