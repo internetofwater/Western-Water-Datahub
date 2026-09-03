50dc729bc3963890d9bade98f4475d37b2744f73
John Salman <31483839+john-salman@users.noreply.github.com>
2026-09-03 12:58:06 -0700

    Update panel styles, bump version (#469)
    * Update panel styles, bump version
    
    * Adjust struture and styles

--- hub-ui/src/features/Panel/index.tsx
+++ hub-ui/src/features/Panel/index.tsx
@@ -3,56 +3,41 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { useEffect, useState } from "react";
-import { Box, Divider, Group, Image, Stack } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
-import { Badge } from "@/components/Badge";
-import { Collection } from "@/features/Panel/Collection";
-import { Datasources } from "@/features/Panel/Datasources";
-import Filters from "@/features/Panel/Filters";
-import { Header } from "@/features/Panel/Header";
-import styles from "@/features/Panel/Panel.module.css";
-import Refine from "@/features/Panel/Refine";
-import loadingManager from "@/managers/Loading.init";
-import mainManager from "@/managers/Main.init";
-import notificationManager from "@/managers/Notification.init";
-import useMainStore from "@/stores/main";
-import useSessionStore from "@/stores/session";
-import {
-  ELoadingType,
-  ENotificationType,
-  EOverlay,
-} from "@/stores/session/types";
+import { useEffect, useState } from 'react';
+import { Box, Divider, Group, Image, Stack } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+import { Collection } from '@/features/Panel/Collection';
+import { Datasources } from '@/features/Panel/Datasources';
+import Filters from '@/features/Panel/Filters';
+import { Header } from '@/features/Panel/Header';
+import styles from '@/features/Panel/Panel.module.css';
+import Refine from '@/features/Panel/Refine';
+import loadingManager from '@/managers/Loading.init';
+import mainManager from '@/managers/Main.init';
+import notificationManager from '@/managers/Notification.init';
+import useMainStore from '@/stores/main';
+import useSessionStore from '@/stores/session';
+import { ELoadingType, ENotificationType, EOverlay } from '@/stores/session/types';
 
 const Panel: React.FC = () => {
   const categories = useMainStore((state) => state.categories);
   const provider = useMainStore((state) => state.provider);
   const overlay = useSessionStore((state) => state.overlay);
 
-  const mobile = useMediaQuery("(max-width: 899px)");
+  const mobile = useMediaQuery('(max-width: 899px)');
 
   const [isVisible, setIsVisible] = useState(true);
 
   const getCollections = async () => {
-    const loadingInstance = loadingManager.add(
-      "Updating data sources",
-      ELoadingType.Collections,
-    );
+    const loadingInstance = loadingManager.add('Updating data sources', ELoadingType.Collections);
     try {
       await mainManager.getCollections();
       loadingManager.remove(loadingInstance);
-      notificationManager.show(
-        "Updated data sources",
-        ENotificationType.Success,
-      );
+      notificationManager.show('Updated data sources', ENotificationType.Success);
     } catch (error) {
       if ((error as Error)?.message) {
         const _error = error as Error;
-        notificationManager.show(
-          `Error: ${_error.message}`,
-          ENotificationType.Error,
-          10000,
-        );
+        notificationManager.show(`Error: ${_error.message}`, ENotificationType.Error, 10000);
       }
       loadingManager.remove(loadingInstance);
     }
@@ -73,21 +58,15 @@ const Panel: React.FC = () => {
   return (
     <>
       {mobile && isVisible && <Box className={styles.panelUnderlay} />}
-      <Box
-        className={styles.panelWrapper}
-        style={{ display: isVisible ? "block" : "none" }}
-      >
-        <Badge />
-
-        <Stack justify="space-between" h="100%">
+      <Box className={styles.panelWrapper} style={{ display: isVisible ? 'block' : 'none' }}>
+        <Stack justify="flex-start" className={styles.panelContent}>
+          <Header />
           <Stack
             gap="calc(var(--default-spacing) * 2)"
             px="calc(var(--default-spacing) * 2)"
-            py="calc(var(--default-spacing) * 3)"
+            pb="calc(var(--default-spacing) * 3)"
             justify="center"
-            className={styles.panelContent}
           >
-            <Header />
             <Stack gap="calc(var(--default-spacing) * 2)">
               <Filters />
               <Divider />
